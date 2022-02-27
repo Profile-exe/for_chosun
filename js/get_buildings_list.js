c4a74d72b9c5114f946531e9edf9d6234a4b942e
Profile-exe <kang67572346@gmail.com>
2022-02-27 22:44:47 +0900

    Update get_buildings_list.js

--- js/get_buildings_list.js
+++ js/get_buildings_list.js
@@ -1,82 +1,12 @@
-export const get_buildings_list = () => {
-  return [
-    {
-      name: 'it융합대학(인류 과학기술의 미래)',
+export const get_buildings_list = async () => {
+  const init = {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=utf-8',
     },
-    {
-      name: '1공학관',
-    },
-    {
-      name: '2공학관',
-    },
-    {
-      name: '경상대학(법대)',
-    },
-    {
-      name: '글로벌기숙사',
-    },
-    {
-      name: '미술대학',
-    },
-    {
-      name: '백학기숙사',
-    },
-    {
-      name: '본관 남쪽',
-    },
-    {
-      name: '본관 북쪽',
-    },
-    {
-      name: '본관 중앙',
-    },
-    {
-      name: '사회대',
-    },
-    {
-      name: '서석홀',
-    },
-    {
-      name: '솔마루',
-    },
-    {
-      name: '약학대학 1호관',
-    },
-    {
-      name: '약학대학 2호관',
-    },
-    {
-      name: '약학대학 3호관',
-    },
-    {
-      name: '의과대학 1호관',
-    },
-    {
-      name: '의과대학 2호관',
-    },
-    {
-      name: '의과대학 3호관',
-    },
-    {
-      name: '자연대',
-    },
-    {
-      name: '정문',
-    },
-    {
-      name: '체대',
-    },
-    {
-      name: '치과대학',
-    },
-    {
-      name: '학군단',
-    },
-    {
-      name: '해오름관',
-    },
-    {
-      name: '후문',
-    },
-  ]
+  }
+
+  const res = await fetch('../data/buildings.json', init)
+  const data = await res.json()
+  return data.buildings
 }

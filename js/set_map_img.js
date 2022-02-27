7acc6757173884beb78342f8918d1c98c977b113
Profile-exe <kang67572346@gmail.com>
2022-02-27 23:43:51 +0900

    rebuild

--- js/set_map_img.js
+++ js/set_map_img.js
@@ -10,14 +10,14 @@ function set_map_img() {
 
   let img_url = ''
 
-  if (depart === '' || arrive === '') {
+  if (depart === '' && arrive === '') {
+    img_url = '../img/chosun_logo.png'
+  } else if (depart === '' || arrive === '') {
     img_url = '../img/404_not_found.png'
+  } else if (depart === arrive) {
+    img_url = '../img/choose_same_place.png'
   } else {
     img_url = `../img/${is_checked}/${depart}_${arrive}_${is_checked}.png`
-  }
-
-  if (depart === arrive) {
-    img_url = '../img/choose_same_place.png'
   }
 
   const img_tag = document.getElementById('map-img')
